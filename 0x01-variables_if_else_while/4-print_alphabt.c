7cd94f4f62c560eb6688c7b27d9b623f66002d43
BekaHabesha <bereketdm1984@gmail.com>
2023-08-12 01:14:39 +0300

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /**
-  *main - Entry point
-  *Description 'Print alphabets except q and e'
-  *Return: Always 0
-  */
+*main - Entry point
+*Description 'Print alphabets except q and e'
+*Return: Always 0
+*/
 int main(void)
 {
-	int i;
+int n = 97;
 
-	for (i = 97; i < 123; i++)
-	{
-		if (i != 101 && i != 113)
-		{
-                i++;
-		continue;
-		}
-		putchar(i);
-		i++;
-		}
-		putchar('\n');
-		return (0);
-	}
+while (n <= 122)
+{
+if (n == 101 || n == 113)
+{
+n++;
+continue;
+}
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
