df1d4f9d6506574955b1fa096c603f8b2ac5918c
BekiHabesha <bereketdm1984@gmail.com>
2023-08-11 04:41:33 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-*main- entry poit
-*Return: always 0 (success)
-*/
+ *main- entry point
+ *Return: always 0 (success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");   
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
