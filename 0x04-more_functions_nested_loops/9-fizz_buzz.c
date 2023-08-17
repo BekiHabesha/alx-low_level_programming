6ba6491bdab0ee1366e2f911783aeab17b600533
BekiHabesha <bereketdere@gmail.com>
2023-08-17 15:45:38 -0700

    9-fizz_buzz.c-Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,26 @@ int i;
 
 for (i = 1 ; i <= 100 ; i++)
 {
-if ((i % 3 == 0) && (i % 5 == 0))
-printf("%s", "FizzBuzz ");
-else if (i % 3 == 0)
-printf("%s", "Fizz ");
-else if (i % 5 == 0)
-printf("%s", "Buzz ");
+if (i % 3 == 0 && i % 5 != 0)
+printf(" Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (i == 1)
+{
+printf("%d", i);
+}
 else
-printf("%d ", i);
+{
+printf(" %d", i);
 }
 printf("\n");
+
 return (0);
 }
