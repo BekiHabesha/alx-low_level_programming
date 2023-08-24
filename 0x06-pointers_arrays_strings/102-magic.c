fed947c2a009e3fdcb7ff3935e648bd93fc1c1c7
BekiHabesha <bereketdere@gmail.com>
2023-08-24 08:54:37 -0700

    102-magic.c - 10. A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
-/**
-* main - program that generates random valid
-* passwords for the program 101-crackme
-*
-* Return: Always 0 (Success)
-*/
-
 int main(void)
 {
 int n;
@@ -15,8 +8,17 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-
+/**
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - You are not allowed to code anything else than this line of code
+*/
 *(p + 5) = 98;
+/* ... so that this prints 98\n */
+
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
