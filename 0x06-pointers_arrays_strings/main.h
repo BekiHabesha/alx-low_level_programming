ffbf52986b3e862aa3942c913dd7bbe914aea3bf
BekiHabesha <bereketdere@gmail.com>
2023-08-24 11:31:27 -0700

    103-infinite_add.c - 11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -21,8 +21,11 @@ char *rot13(char *);
 char *rot13(char *a);
 void print_number(int n);
 char *_infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_array(int *a, int n);
 char *string_toupper(char *n);
+void rev_string(char *n);
+void rev_string(char *);
+void print_buffer(char *b, int size);
 
 #endif
