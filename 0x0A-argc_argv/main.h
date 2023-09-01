a0189f8377dc6d892e2c9d5a38e6ebdda8977fca
BekaHabesha <bereketdm1984@gmail.com>
2023-09-02 01:18:02 +0300

    main head file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+
+int _putchar(char c);
+void _puts(char *s);
+int main (int argc, char *argv[]);
+int main (int argc __attribute__((unused)), char *argv[])
