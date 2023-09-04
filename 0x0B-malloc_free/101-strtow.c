81040e7542f8d0e7eec6f0abe5ab1b038b0022b6
BekaHabesha <bereketdm1984@gmail.com>
2023-09-05 02:29:42 +0300

    0x06-pointers_arrays_strings project

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,9 +16,8 @@ char **strtow(char *str);
 int word_len(char *str)
 {
 	int index = 0, len = 0;
-	char *s;
 
-	while ((*(str + index) && (str + index)) != ' ')
+	while (*(str + index) && *(str + index) != ' ')
 	{
 		len++;
 		index++;
@@ -64,37 +63,41 @@ char **strtow(char *str)
 	char **strings;
 	int index = 0, words, w, letters, l;
 
-	if (str == NULL || str == '\0')
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
 	words = count_words(str);
 	if (words == 0)
 		return (NULL);
 
-	strings = malloc((words + 1) * (sizeof(char*)));
-	if (strings == 0)
-		return (NULL);
+	strings = malloc(sizeof(char*) * (words + 1));
+			if (strings == 0)
+			return (NULL);
 
-	for (w = 0; w < words; w++)
-	{
-		while (str[index] == ' ')
+			for (w = 0; w < words; w++)
+			{
+			while (str[index] == ' ')
 			index++;
 
-		letters = word_len(str + index);
+			letters = word_len(str + index);
 
-		strings[w] = malloc(sizeof(char) * (letters + 1));
+			strings[w] = malloc(sizeof(char) * (letters + 1));
 
-		if (strings[w] == NULL)
-		{
+			if (strings[w] == NULL)
+			{
 			for (; w >= 0; w--)
-				return (NULL);
-		}
+			free(strings[w]);
 
-		for (l = 0; l < letters; l++)
-			strings[w][l] = str[index++];
+			free(strings);
+			return (NULL);
+			}
 
-		strings[w][l] = '\0';
-	}
+			for (l = 0; l < letters; l++)
+				strings[w][l] = str[index++];
+
+			strings[w][l] = '\0';
+			}
+	strings[w] = NULL;
 
 	return (strings);
 }
