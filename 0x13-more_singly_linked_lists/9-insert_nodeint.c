3c2cd6190051be26767f4859447f0ffc48cfbadd
BekiHabesha <bereketdere@gmail.com>
2023-09-26 03:21:55 +0300

    9-insert_nodeint.c - 9. Insert.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,45 +2,67 @@
 #include <stdlib.h>
 
 /**
-  * insert_nodeint_at_index - inserts a new node in a linked list.
-  * at a given position
-  * @head: pointer to the first node in the list
-  * @idx: index where the new node is added
-  * @n: data to insert in the new node
+  * create_new_node - creates a new node
+  * @n: the data of the node
+  * Return: pointer to the node
+  */
+listint_t *create_new_node(int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
+  * insert_nodeint_at_index - insert a node at an index in a list.
+  * @head: pointer to the first element of the list
+  * @idx: index to the position where the new node is
+  *       to be inserted and added
+  * @n: the data of the new node created
   *
-  * Return: pointer to the new node, or NULL
+  * Return: the pointer address of the newly created node, or NULL
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *tmp;
+	listint_t *tmp_old;
+	listint_t *new_node;
 
-	new = malloc(sizeof(listint_t));
-	if (!new == !head)
+	tmp = *head;
+	if (head == NULL)
 		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
+	new_node = create_new_node(n);
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		*head = new_node;
 	}
-
-	for (i = 0; temp && i < idx; i++)
+	for (i = 0; i < idx - 1 && tmp != NULL && idx != 0; i++)
+		tmp = tmp->next;
+	if (tmp == NULL)
+		return (NULL);
+	if (idx == 0)
+		new_node->next = tmp;
+	else
 	{
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
+		tmp_old = tmp->next;
+		tmp->next = new_node;
+		new_node->next = tmp_old;
 	}
 
-	return (NULL);
+	return (new_node);
 }
