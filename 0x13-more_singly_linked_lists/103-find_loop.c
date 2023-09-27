9999eacf6172ace7f15728411a9b38e1c31c4c20
BekiHabesha <bereketdere@gmail.com>
2023-09-27 11:48:19 -0700

    103-find_loop.c - 14. Find the loop.

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,42 +1,39 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
-* find_listint_loop - Finds the loop contained in
-*                     a listint_t linked list.
-* @head: A pointer to the head of the listint_t list.
-*
-* Return: If there is no loop - NULL.
-* otherwise - the address of the node where the loop starts.
+* find_listint_loop - finds the loop contained
+*                     in a linked list
+* @head : pointer to the head of the linked list
+* Return: pointer the node where the loop starts,
+*         or NULL - if there is no loop
 */
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *tortoise;
-listint_t *hare;
+listint_t *node1, *node2;
 
 if (head == NULL || head->next == NULL)
 return (NULL);
 
-tortoise = head->next;
-hare = (head->next)->next;
+node1 = head->next;
+node2 = (head->next)->next;
 
-while (hare)
+while (node2)
 {
-if (tortoise == hare)
+if (node1 == node2)
 {
-tortoise = hare;
+node1 = head;
 
-while (tortoise != hare)
+while (node1 != node2)
 {
-tortoise = tortoise->next;
-hare = hare->next;
+node1 = node1->next;
+node2 = node2->next;
 }
 
-return (tortoise);
+return (node1);
 }
 
-tortoise = tortoise->next;
-hare = (hare->next)->next;
+node1 = node1->next;
+node2 = (node2->next)->next;
 }
 
 return (NULL);
