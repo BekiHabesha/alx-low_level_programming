d1a62123212157a4a2fdfd4b6ee3e979183b1fc8
BekiHabesha <bereketdere@gmail.com>
2023-09-29 14:38:55 -0700

    _putchar.c file - 0x14. C - Bit manipulation _putchar.c file created

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,11 +1,3 @@
-/*
-* File_name: _putchar.c file
-* Created: 29th of Septrmber, 2023
-* Auth: Bereket Dereje Mekonnen
-* GitHub repository: alx-low_level_programming
-* Project: 0x14. C - Bit manipulation.
-*/
-
 #include "main.h"
 #include <unistd.h>
 
