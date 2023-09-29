e7e6252a8e4aaaab5f751b6d76a54e974fcde258
BekiHabesha <bereketdere@gmail.com>
2023-09-29 14:12:16 -0700

    0-binary_to_uint.c - 0. Print list.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,7 +11,7 @@
 
 int _putchar(char c);
 int _atoi(const char *s);
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
