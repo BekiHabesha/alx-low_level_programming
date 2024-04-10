e8308e90d5fb759c8536641f7310c175c8f1216e
BekiHabesha <bereketdere@gmail.com>
2024-04-10 07:42:53 -0700

    No.9.Interpolation search.

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,77 @@
+/*
+ * File_name: 102-interpolation.c
+ * Task_no: 9
+ * Task_Title: Interpolation search
+ *
+ * Functions: int interpolation_search(int *array, size_t size, int value);
+ * Created: On April 10, 2024
+ * Author: Bereket Dereje Mekkonen
+ * Student ID: 159650
+ *
+ * GitHub repository: alx-low_level_programming
+ * Project Title: 0x1E. C - Search Algorithms
+ * Directory: 0x1E-search_algorithms
+ *
+ * Curriculum: ALX SE Foundations
+ * Chohort: 19
+ * Program: Full Stack Software Engineering | Founder Academy
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in an array of integers
+ *                       using the Interpolation search algorithm
+ *
+ * Author: Bereket Dereje Mekkonen
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ *
+ * Return: index of the number
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, low, high;
+	double f;
+
+	if (array == NULL)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+
+	while (size)
+	{
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
+
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
+		else
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
+	}
+
+	return (-1);
+}
